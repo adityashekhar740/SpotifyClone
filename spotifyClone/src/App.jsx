e16dc5364aca3555f07a03f66e1b4b36c82fdfd1
adityashekhar740 <shekharaditya740@gmail.com>
2024-06-26 12:41:13 +0530

    done all

--- spotifyClone/src/App.jsx
+++ spotifyClone/src/App.jsx
@@ -3,6 +3,7 @@ import LandingPage from './components/LandingPage'
 import Callback from './components/Callback'
 import AlbumPage from './pages/AlbumPage'
 import Layout from './components/Layout'
+import GenrePage from './pages/GenrePage'
 
 const App = () => (
   <Router>
@@ -11,6 +12,7 @@ const App = () => (
         <Route path="/" element={<LandingPage />} />
         <Route path="/callback" element={<Callback />} />
         <Route path='/album' element={<AlbumPage />} />
+        <Route path='/genre' element={<GenrePage />} />
       </Route>
     </Routes>
   </Router>
